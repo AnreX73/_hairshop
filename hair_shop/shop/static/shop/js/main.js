5c2139867fcf79583e12524821e520a392567b39
AnreX <han220173@gmail.com>
2026-03-19 21:30:09 +0700

    add swiper

--- hair_shop/shop/static/shop/js/main.js
+++ hair_shop/shop/static/shop/js/main.js
@@ -1,44 +1,25 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     console.log('Main.js loaded');
-    
+document.addEventListener('DOMContentLoaded', function() {
 
-// function updatemenu() {
-//   if (document.getElementById('responsive-menu').checked == true) {
-//     document.getElementById('menu').style.borderBottomRightRadius = '0';
-//     document.getElementById('menu').style.borderBottomLeftRadius = '0';
-//   }else{
-//     document.getElementById('menu').style.borderRadius = '10px';
-//   }
-// }
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-// gsap.registerPlugin(ScrollTrigger);
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-// const tl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".categories",
-//     start: "-250px",
-//     end: "150px",    // Завершить анимацию через 150px скролла
-//     scrub: 1       // Мягкая привязка к скроллу
-//   }
-// });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-// tl.to("header", { 
-//   height: "60px",
-//   padding: "10px 20px" // Уменьшаем внутренние отступы
-// }, 0) // "0" означает начало анимации одновременно с запуском таймлайна
+  // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+});
 
-// .to(".logo_container > img", { 
-//   scaleX: -0.5,
-//   scaleY: 0.5,
-//   x:-300,
-//   y:-60,       // Уменьшаем логотип на 30%
-//   duration: 1
-// }, 0)
-// .to(".slogan_wrapper", { 
-//   x:1000,
-//   opacity: 0,        // Уменьшаем логотип на 30%
-//   duration: 1
-// }, 0)
-
-
-// });
+});
